1d4a501a13ac57db5b9c07e3449515ea152d94df
hakimxiao <alhakimabusolihin@gmail.com>
2025-09-07 20:14:45 +0700

    - DATABASE SEEDER / SEEDING DATABASE + COMBO FACTORY

--- README.md
+++ README.md
@@ -31,6 +31,8 @@
 - App\Models\Post::factory(20)->recycle(User::factory(5)->create())->create();
 - * user nya hanya 5 dan post nya 20 maka 20 post akan disesuaikan ke 5 user
 
+- SEEDER 
+- MEMBUAT SEDEER : php artisan make:seeder
 
 ## JIKA MENEMUKAN MASALAH SAAT JALANAKAN FACTORY BERUNTUN SEPERTI INI MAKA JALANKAN 2 TAHAP INI : 
 App\Models\Post::factory(20)->recycle([Category::factory(3)->create(), User::factory(5)->create()])->create()
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,50 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+// @_CONTOH NGESDIDING ATAU MENANAM SATU DATA MANUAL :
+        
+    // * TIPS : Jika kita melakukkan siding tapi ada data yang ingin dipertahankan, maka bisa menggunakan ini : 
+    //           - simpan di variabel kemudian di ryclce di factory.
+        // $membership = User::create([
+        //     'name' => 'Herdian',
+        //     'username' => 'herdian',
+        //     'email' => 'herian@gmail.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('password'), // password
+        //     'remember_token' => Str::random(10)
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Programming',
+        //     'slug' => 'web-programming'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'judul artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. dolor sit amet consectetur adipisicing elit.'
+        // ]);
+
+// @_ MENGGUNAKAN FACTORY DI SEEDING | LEBIH MUDAH DARI PADA TINKER
+        // Post::factory(20)->recycle([
+            
+        //     $membership,
+        //     // User::factory(5)->create(),
+        // ])->create();
+
+// @_ MELAKUKKAN SEEDING PADA CLASS SEEDING YANG BERBEDA 
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+
+        Post::factory(20)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
