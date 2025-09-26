abcf9ba9026fb5ede847a90552080f0e7053fde3
hakimxiao <alhakimabusolihin@gmail.com>
2025-09-26 23:32:18 +0700

    - Membuat Pagination -
    * Penghalamanan page
    - Ini yang sederhana

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
 
     
-    return view('posts', ['title' => 'Blog Page', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(6)]);
 });
 
 
