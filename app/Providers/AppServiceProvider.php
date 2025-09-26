a48c10f9ed3e73b8cf7515a1c644f913fa0a67b3
hakimxiao <alhakimabusolihin@gmail.com>
2025-09-26 23:48:10 +0700

    - CUSTOM PAGINATION VENDOR -
    * mengenal vitur vendor publkish fitur yang digunakan untuk mengedit maupun mendaftarkan desain baru dari framework yang ada dilaravel dan jika selesai di setup didalam app/providers AppServiceProvider pada bagian boot
    
    * Mmengatasi masalah jika kita melakukkan query dengan pagination agar tidak ngilang dan jalan sesuai dengan pagination adalah dengan withQueryString.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Model::preventLazyLoading();
+        Paginator::useTailwind();
     }
 }
